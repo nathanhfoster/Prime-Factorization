7ca1087279de56656a336116173b3e98b585bdea
Nathan <nateinthegame@gmail.com>
2017-01-30 11:52:50 -0800

    Initial publication
    Visual studio project that contains an automated recursive algorithm
    written in C++; it prints all the prime factorizations of randomly
    generated numbers.

---
+++ Recursive Prime Factorization Automation/Watch.hpp
@@ -0,0 +1,23 @@
+#ifndef WATCH_HPP
+#define WATCH_HPP
+#include<iostream>
+#include <thread> /* Sleep */
+template<typename TimeT = std::chrono::microseconds,
+	typename ClockT = std::chrono::high_resolution_clock,
+	typename DurationT = double>
+	class Watch
+{
+private:
+	std::chrono::time_point<ClockT> _start, _end;
+public:
+	Watch() { start(); }
+	void start() { _start = _end = ClockT::now(); }
+	void sleep(int t) { std::this_thread::sleep_for(std::chrono::milliseconds(t)); }
+	DurationT stop() { _end = ClockT::now(); return elapsed(); }
+	DurationT elapsed() {
+		auto delta = std::chrono::duration_cast<TimeT>(_end - _start);
+		return delta.count();
+	}
+};
+#endif
+// std::this_thread::sleep_for(std::chrono::milliseconds(5000));
---
+++ Recursive Prime Factorization Automation/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include "Random.hpp"
+#include "Watch.hpp"
+#include <stdio.h> 
+#include <time.h>
+#include <chrono> // Time
+#include <thread> // Sleep
+#include <iomanip>
+
+void primeFactorization(int n, int factor);
+
+// Random number generator instance
+Random r;
+
+
+void main(){
+	// Start watch
+	Watch<> w;
+	int autoTotal = 1000;
+
+	// Automate random numbers
+	for (int i = 0; i < autoTotal; i++) {
+		int number = r.DrawNumber(1, autoTotal);
+
+		std::cout << "Number: " << number << "\tPrime factors: ";
+
+		primeFactorization(number, 2);
+
+		std::cout << "\n";
+	}
+	// Stop watch
+	w.stop();
+	std::cout << std::setprecision(4) << "Total time elapsed in s: " << w.elapsed() * pow(10, -6) << std::endl;
+	
+	system("PAUSE");
+}
+
+void primeFactorization(int n, int factor = 2)
+{
+	// Base case
+	if (n < 2)
+		return;
+	// If factor divies number evenly
+	if (n % factor == 0){
+		primeFactorization(n / factor, factor);
+		std::cout << factor << "\t";
+	}
+	// Else increment the factor
+	else
+		primeFactorization(n, factor + 1);
+}
